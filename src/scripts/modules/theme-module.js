1a5d3aac9041cd88a55d248e91da06f31ef82642
robinShoreD <robin.b@shoredigitalinc.com>
2024-05-08 14:52:52 +0800

    video gallery

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -13,7 +13,41 @@ AppName.Modules.ThemeModule = (function () {
         el: '.swiper-pagination',
       },
     });
+
   };
+
+  // Slick on Video Gallery
+  $('.testimonials-container').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:"<a href='#' class='slick-arrow slick-prev'><img src='/assets/images/slick_left.png'></a>",
+    nextArrow:"<a href='#' class='slick-arrow slick-next'><img src='/assets/images/slick_right.png'></a>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
 
   /////////////////////
   // Public Methods //
@@ -26,3 +60,4 @@ AppName.Modules.ThemeModule = (function () {
     init: init,
   };
 })();
+
