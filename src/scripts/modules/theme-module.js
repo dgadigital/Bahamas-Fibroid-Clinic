8ee2a2cfa81f68a00608752779a4b8f8f11a12b8
leopaul-fk <leo@firekamp.com>
2024-05-09 10:27:26 +0800

    ...

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -15,11 +15,22 @@ AppName.Modules.ThemeModule = (function () {
     });
   };
 
+
+  var _instafeed = () => {
+    var feed = new Instafeed({
+      accessToken: 'IGQWRNYUZAmY0I1V18zRnNvRGtiZAmQ0cmIwU3ZANbHNEOTVpVXdKNlVCaWdMZAW9MR3ZAzcnV3ZA2xDQjZAzNVBkaUhQRENPaV9zVnM3aU5OaVN2ZA3JEMTVxT2pxSlh6dV9JUXBWYm5pZAElia3BGSUNOOG53WkJ4YlZARMzQZD',
+      template: '<div class="post image-wrapper"><a href="{{link}}" target="_blank"><img src="{{image}}"></a></div>',
+      limit: 6
+    });
+    feed.run();
+  }
+
   /////////////////////
   // Public Methods //
   ///////////////////
   const init = function () {
     _privateMethod();
+    _instafeed();
   };
 
   return {
