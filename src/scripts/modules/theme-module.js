55c20230cb3847682b3967101db5f8da708142af
robinShoreD <robin.b@shoredigitalinc.com>
2024-05-15 08:40:59 +0800

    update

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -85,6 +85,36 @@ AppName.Modules.ThemeModule = (function () {
         // instead of a settings object
       ]
     });
+
+    $('.testimonial-text-container-video-list').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow:"<a href='#' class='slick-arrow slick-prev'><img src='assets/images/slick_left.png'></a>",
+      nextArrow:"<a href='#' class='slick-arrow slick-next'><img src='assets/images/slick_right.png'></a>",
+      responsive: [
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
   }
 
 
